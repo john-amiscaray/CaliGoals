1165529600e300571a911595344c081373462c9d
Hanna Frances <50343069+wavy2022@users.noreply.github.com>
2021-01-01 15:13:42 -0500

    Added tool bar.

--- front-end/main.py
+++ front-end/main.py
@@ -1,5 +1,6 @@
 # main.py
 import PySimpleGUI as sg
+from PySimpleGUI import InputCombo, Combo, Multiline, ML, MLine, Checkbox, CB, Check, Button, B, Btn, ButtonMenu, Canvas, Column, Col, Combo, Frame, Graph, Image, InputText, Input, In, Listbox, LBox, Menu, Multiline, ML, MLine, OptionMenu, Output, Pane, ProgressBar, Radio, Slider, Spin, StatusBar, Tab, TabGroup, Table, Text, Txt, T, Tree, TreeData,  VerticalSeparator, Window, Sizer
 
 '''
 format:
@@ -14,21 +15,38 @@ format:
 
 #----- sublayouts -----#
 
+# layout for the login page
 login_page_layout = [
-    [sg.Image(r'cat.png')],
-    [sg.Text('Slogan'), sg.Input(), sg.Text("I love waffles")]
+    [Image(r'cat.png')],
+    [Text('Slogan'), Input()]
 ]
 
-cheese = 'cheese'
+# layout for the info bar at top
+top_bar_layout = [
+    [Button('Friends'), Button('USERNAME_PLACEHOLDER'), Image(r'picture_placeholder.png')] # top row of the profile
+]
+
+# layout for the badges
+badges_layout = [
+    [Listbox]
+]
+
+my_profile_layout = [
+    [top_bar_layout],
+    [Image(r'picture_placeholder.png'), badges_layout, goals_layout]
+]
+
+
 
 #----- layout -----#
 
 # where we put all the page layouts together as columns
 layout = [
-    [sg.Column(login_page_layout, visible=True)]
+    [Column(login_page_layout, visible=False)],
+    [Column(top_bar_layout, visible=True)],
 ]
 
-window = sg.Window('login test', layout)
+window = Window('login test', layout)
 
 #----- events -----#
 
@@ -37,6 +55,8 @@ while True:
 
     if event == sg.WIN_CLOSED:
         break
+    elif event == 'Friends':
+        print("clicked on friends")
 
 
 window.close()
