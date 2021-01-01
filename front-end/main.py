ffd657f2f255f2a39afab06f952133e2374ff402
Johan George <44176815+Johan-George@users.noreply.github.com>
2021-01-01 15:18:56 -0500

    changes to the imports

--- front-end/main.py
+++ front-end/main.py
@@ -1,6 +1,7 @@
 # main.py
 import PySimpleGUI as sg
-
+#---- delete unused-----#
+from PySimpleGUI import InputCombo, Combo, Multiline, ML, MLine, Checkbox, CB, Check, Button, B, Btn, ButtonMenu, Canvas, Column, Col, Combo, Frame, Graph, Image, InputText, Input, In, Listbox, LBox, Menu, Multiline, ML, MLine, OptionMenu, Output, Pane, ProgressBar, Radio, Slider, Spin, StatusBar, Tab, TabGroup, Table, Text, Txt, T, Tree, TreeData,  VerticalSeparator, Window, Sizer
 '''
 format:
 - functions
@@ -15,8 +16,8 @@ format:
 #----- sublayouts -----#
 
 login_page_layout = [
-    [sg.Image(r'cat.png')],
-    [sg.Text('Slogan'), sg.Input()]
+    [Image(r'cat.png')],
+    [Text('Slogan'), InputText()]
 ]
 
 
@@ -24,10 +25,10 @@ login_page_layout = [
 
 # where we put all the page layouts together as columns
 layout = [
-    [sg.Column(login_page_layout, visible=True)]
+    [Column(login_page_layout, visible=True)]
 ]
 
-window = sg.Window('login test', layout)
+window = Window('login test', layout)
 
 #----- events -----#
 
