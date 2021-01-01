e2d6e6fcf3b7be5f3418c16303422b0a25a342c4
john-amiscaray <john_ernest_amiscaray@outlook.com>
2021-01-01 10:23:57 -0500

    init repo

---
+++ back-end/src/main/java/io/caligoals/caligoals/CaliGoalsApplication.java
@@ -0,0 +1,13 @@
+package io.caligoals.caligoals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaliGoalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaliGoalsApplication.class, args);
+    }
+
+}
---
+++ back-end/src/test/java/io/caligoals/caligoals/CaliGoalsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.caligoals.caligoals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaliGoalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
